3a9c2fc2d5289660f7ab16fc10ee1df60f96c459
子不语怪力乱神 <email619@163.com>
2024-01-30 17:28:32 +0800

    Add files via upload

---
+++ ClassFlowLayout.swift
@@ -0,0 +1,34 @@
+//
+//  ClassFlowLayout.swift
+//  ZiYe-iOS
+//
+//  Created by APPLE on 2024/1/30.
+//
+
+import Foundation
+
+/// 让内容左对齐
+class ClassFlowLayout: UICollectionViewFlowLayout {
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let attributes = super.layoutAttributesForElements(in: rect) else { return nil }
+        
+        for (i, attribute) in attributes.enumerated() {
+            if attribute.representedElementKind == nil {
+                if let lastAttribute = attributes[safe: i - 1] {
+                    if attribute.frame.minX - lastAttribute.frame.maxX > minimumInteritemSpacing {
+                        attribute.frame.origin.x = lastAttribute.frame.maxX + minimumInteritemSpacing
+                    }
+                }
+            }
+        }
+        return attributes
+    }
+}
